3dc6d4ad04f22683502363b1c26b152574ca7439
GiorgioGhisotti <g.ghisotti@gmail.com>
2019-09-21 21:15:12 +0200

    Top artists, songs

---
+++ scripts/songs.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+# Get all songs from given artists
+
+import argparse
+import lyricsgenius
+import json
+import sys
+import os
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--data", help="Input data")
+    parser.add_argument("-o", "--output", help="Set output file")
+    parser.add_argument("-g", "--genius", help="Set genius access token")
+    parser.add_argument("-n", "--number", help="Set number of artists")
+    args = parser.parse_args()
+
+    access_token = args.genius if args.genius else "JW59dXoj6pHvcaHKGyUTpPwOl2FNQNfYuwZXvVFyJv7xWjs48UEQMD0sWm2jnJkp"
+    path = args.output if args.output else "song_lyrics.json"
+    data = args.data if args.data else "artists.json"
+    num = int(args.number) if args.number else 15
+
+    try:
+        genius = lyricsgenius.Genius(access_token)
+    except Exception as e:
+        print("%s, could not sign in to Genius." % (e))
+        sys.exit(1)
+
+    try:
+        os.makedirs(path, exist_ok=True)
+    except Exception as e:
+        print("%s, could not create folder at requested path!" % (e))
+        sys.exit(1)
+
+    top_artists = []
+    with open(data, "r") as df:
+        top_artists = json.load(df)
+
+    genius_artists = []
+    for artist in top_artists[:num]:
+        genius_artists.append(
+            genius.search_artist(artist["name"], max_songs=10000)
+        )
+    genius.save_artists(artists=genius_artists, filename=path)
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/top_artists.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import json
+import argparse
+
+
+def sort_artists(data):
+    artists = []
+    for song in data:
+        insert = True
+        for artist in artists:
+            if artist["name"] == song["artist"]:
+                artist["appearances"] += song["billboard"]
+                insert = False
+        if insert:
+            artists.append(
+                {"name": song["artist"], "appearances": song["billboard"]}
+            )
+    artists = sorted(artists, key=lambda artist: artist["appearances"])
+    artists.reverse()
+    return artists
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--data", help="Data file")
+    parser.add_argument("-o", "--output", help="Output file")
+    args = parser.parse_args()
+
+    if not args.data:
+        print("Please provide the path to a valid data file.")
+
+    data_file = args.data
+    output_path = args.output if args.output else "artists.json"
+    data = []
+    with open(data_file, "r") as df:
+        data = json.load(df)
+    data = sorted(data, key=lambda song: song["billboard"])
+    data.reverse()
+
+    output = sort_artists(data)
+    print(output[:10])
+    with open(output_path, "w") as of:
+        json.dump(output, of)
+
+
+if __name__ == "__main__":
+    main()
