a94be54a446123bce3b6f8ce267216ccc87d63eb
GiorgioGhisotti <g.ghisotti@gmail.com>
2019-10-05 19:18:43 +0200

    means

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -100,7 +100,7 @@ def main():
             ) for corpus in text["songs"]
         ] for text in texts if text["artist"] == "Drake"
     ]
-    print(means)
+    print("means: %d, %d" % (len(means), len(means[0])))
     a = model.most_similar(means[0], topn=1)
     b = model.most_similar(means[1], topn=1)
     print(model.distance(a, b))
